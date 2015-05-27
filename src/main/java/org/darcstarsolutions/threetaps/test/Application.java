3a39233a1e554dbc7165bebdd9a9f5d97c413280
mharris021 <mharris021@hotmail.com>
2015-05-26 21:08:01 -0500

    initial commit

---
+++ src/main/java/org/darcstarsolutions/threetaps/test/Application.java
@@ -0,0 +1,15 @@
+package org.darcstarsolutions.threetaps.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by mharris on 5/26/15.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
